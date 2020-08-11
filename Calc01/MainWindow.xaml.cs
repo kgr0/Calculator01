0459e1b1939e83ea9ad200e90b953a8dd29836cc
Katia Grytsko <katiagrutzko.2001@gmail.com>
2020-08-11 11:41:33 +0300

    allfunctions without  exception handling and with accepting 08.23 or -0

--- Calc01/MainWindow.xaml.cs
+++ Calc01/MainWindow.xaml.cs
@@ -21,62 +21,86 @@ namespace Calc01
     /// </summary>
     public partial class MainWindow : Window
     {
-        Regex reg1 = new Regex(@"^[1-9]{1}$");
-        Regex reg2 = new Regex(@"^\d{0,5}$");
-        private int znak = 0; //0+ 1- 2* 3/
+        Regex reg = new Regex(@"^[-]?([0]{1})|([1-9]{1}([0-9]{1,7})?)([,][0-9]{1,8})?$");
+      
+        private int operation = 0; //0+ 1- 2* 3/
         public MainWindow()
         {
             InitializeComponent();
+            a_value.Text = "";
+            a_value.Width = this.Width;
+            b_value.Text = "";
+            b_value.Width = this.Width;
+            result.Text = "";
+            result.Width = this.Width;
+
+            Background = new SolidColorBrush(Color.FromRgb(179,201,230));
+            a_value.Background = new SolidColorBrush(Color.FromRgb(194, 211, 235));
+            b_value.Background = new SolidColorBrush(Color.FromRgb(194, 211, 235));
+            result.Background = new SolidColorBrush(Color.FromRgb(179, 201, 230));
         }
 
         private void button_plus_Click(object sender, RoutedEventArgs e)
         {
-            znak = 0;
+            operation = 0;
+            default_button_col();
+            button_plus.Background= new SolidColorBrush(Color.FromRgb(147, 178, 220));
         }
 
         private void button_minus_Click(object sender, RoutedEventArgs e)
         {
-            znak = 1;
+            operation = 1;
+            default_button_col();
+            button_minus.Background = new SolidColorBrush(Color.FromRgb(147, 178, 220));
         }
 
         private void button_multi_Click(object sender, RoutedEventArgs e)
         {
-            znak = 2;
+            operation = 2;
+            default_button_col();
+            button_multi.Background = new SolidColorBrush(Color.FromRgb(147, 178, 220));
         }
 
         private void button_div_Click(object sender, RoutedEventArgs e)
         {
-            znak = 3;
+            operation = 3;
+            default_button_col();
+            button_div.Background = new SolidColorBrush(Color.FromRgb(147, 178, 220));
+        }
+
+        private void default_button_col()
+        {
+            button_plus.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            button_minus.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            button_multi.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            button_div.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
         }
 
         private void button_eq_Click(object sender, RoutedEventArgs e)
         {
-            if (Regex.IsMatch(a_value.Text. )
-
-            if(a_value.Text != "" && b_value.Text != "")
+            if (!(reg.IsMatch(a_value.Text) && reg.IsMatch(b_value.Text)))
             {
-                switch (znak)
+                MessageBox.Show("Invalid input! ");
+                return;
+            }
+
+                switch (operation)
                 {
                     case 0:
-                        result.Text = (Convert.ToInt32(a_value.Text) + Convert.ToInt32(b_value.Text)).ToString();
+                        result.Text = (Convert.ToDouble(a_value.Text) + Convert.ToDouble(b_value.Text)).ToString();
                         break;
                     case 1:
-                        result.Text = (Convert.ToInt32(a_value.Text) - Convert.ToInt32(b_value.Text)).ToString();
+                        result.Text = (Convert.ToDouble(a_value.Text) - Convert.ToDouble(b_value.Text)).ToString();
                         break;
                     case 2:
-                        result.Text = (Convert.ToInt32(a_value.Text) * Convert.ToInt32(b_value.Text)).ToString();
+                        result.Text = (Convert.ToDouble(a_value.Text) * Convert.ToDouble(b_value.Text)).ToString();
                         break;
                     case 3:
-                        result.Text = (Convert.ToInt32(a_value.Text) / Convert.ToInt32(b_value.Text)).ToString();
+                        result.Text = (Convert.ToDouble(a_value.Text) / Convert.ToDouble(b_value.Text)).ToString();
                         break;
 
                 }
-            }
-
-            else
-            {
-                result.Text =  "Error"
-            }
+        
             
             
         }
